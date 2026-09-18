785821bb0484daa1b46c9507b8327e2e4ef77759
Arooba <iftikhar8995@gmail.com>
2026-09-18 15:58:23 +0500

    Update PostgreSQL connection and print job_skills count
    Added SSL mode to PostgreSQL connection string and fixed print statement for job_skills count.

--- job-market-project/scraper/step11_load_to_postgres.py
+++ job-market-project/scraper/step11_load_to_postgres.py
@@ -17,7 +17,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
-connection_string = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+connection_string = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
 engine = create_engine(connection_string)
 
 print("Testing connection...")
